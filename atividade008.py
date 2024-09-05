ca2c1b6925a2c55bb5a496edecf36a07d3a656f3
pedro01p <tsukki237@gmail.com>
2024-09-05 12:46:56 -0300

    conversão de real pra dolar

---
+++ atividade008.py
@@ -0,0 +1,4 @@
+# Imagine que você está em uma viagem para os Estados Unidos e precisa converter o valor em reais para dólares. Crie um programa que receba o valor em reais e a taxa de câmbio atual, e exiba o valor equivalente em dólares.
+real=float(input("informe a quamtidade de dinheiro brasileiro:RS$"))
+dolar=float(real*0.18)
+print(dolar)
